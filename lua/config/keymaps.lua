a937f661b4a7e9442bcfea955728ac2479f1e384
kokimiza <4.togenkyo@gmail.com>
2026-01-07 16:37:33 +0900

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,3 +22,29 @@ vim.keymap.set("n", "<leader>h", ":tabprevious<CR>")
 vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { silent = true, buffer = args.buf }
+
+    vim.keymap.set(
+      "n",
+      "gl",
+      vim.diagnostic.open_float,
+      vim.tbl_extend("force", opts, { desc = "LSP: show diagnostics" })
+    )
+
+    vim.keymap.set(
+      "n",
+      "[d",
+      vim.diagnostic.goto_prev,
+      vim.tbl_extend("force", opts, { desc = "LSP: previous diagnostic" })
+    )
+
+    vim.keymap.set(
+      "n",
+      "]d",
+      vim.diagnostic.goto_next,
+      vim.tbl_extend("force", opts, { desc = "LSP: next diagnostic" })
+    )
+  end,
+})
