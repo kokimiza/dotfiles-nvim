663606ed18e024a4e401a5575bf955bfce2a535c
kokimiza <4.togenkyo@gmail.com>
2026-01-08 16:06:54 +0900

    Configure auto and smart indentation options
    Add indentation settings for better code formatting.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,10 @@ vim.opt.cursorcolumn = true   -- 現在列をハイライト
 -- 通常貼り付けを常にクリップボードからにする
 vim.opt.clipboard = "unnamedplus"
 
+-- インデント設定
+vim.opt.autoindent = true     -- 自動インデント
+vim.opt.smartindent = true    -- スマートインデント
+vim.opt.expandtab = true      -- タブをスペースに変換
+vim.opt.tabstop = 2           -- タブ幅
+vim.opt.shiftwidth = 2        -- インデント幅
+vim.opt.softtabstop = 2       -- ソフトタブ幅
