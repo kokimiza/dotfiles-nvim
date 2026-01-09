ef45e0a7bd8487c3c9eaa7f5d4a772abe51e6163
kokimiza <4.togenkyo@gmail.com>
2026-01-09 13:17:46 +0900

    Enhance keymaps for tabs and window management
    Add key mappings for tab navigation, window splitting, and window movement.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,8 +16,20 @@ vim.keymap.set("t", "jj", [[<C-\><C-n>]], {
   silent = true,
 })
 
+-- タブ移動
 vim.keymap.set("n", "<leader>l", ":tabnext<CR>")
 vim.keymap.set("n", "<leader>h", ":tabprevious<CR>")
+
+-- ウィンドウ分割
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "垂直分割 (Split Vertical)" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "水平分割 (Split Horizontal)" })
+vim.keymap.set("n", "<leader>sc", ":close<CR>", { desc = "ウィンドウを閉じる (Split Close)" })
+
+-- ウィンドウ間移動
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "左のウィンドウに移動" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "下のウィンドウに移動" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "上のウィンドウに移動" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "右のウィンドウに移動" })
 
 vim.opt.timeout = true
 vim.opt.timeoutlen = 300
