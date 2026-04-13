afe7889cdc42bec0ed36d95eb79e31c02b3942b6
kokimiza <4.togenkyo@gmail.com>
2026-04-14 08:52:31 +0900

    Refactor oil.nvim plugin configuration
    Updated configuration for oil.nvim plugin with new options and key mappings.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,22 +1,108 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		lazy = false,
-		cmd = { "Oil" },
-		keys = {
-			{ "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
-		},
-		opts = {
-			default_file_explorer = true,
-			columns = {
-				"icon",
-				"permissions",
-				"size",
-				"mtime",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-		},
-	},
+  {
+    "stevearc/oil.nvim",
+    event = "VimEnter",
+    init = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+    end,
+    cmd = { "Oil" },
+    keys = {
+      { "-", "<cmd>Oil<CR>", desc = "親ディレクトリを開く" },
+      { "<leader>e", "<cmd>Oil<CR>", desc = "ファイルエクスプローラーを開く" },
+      { "<leader>E", "<cmd>Oil --float<CR>", desc = "フローティングエクスプローラー" },
+    },
+    opts = {
+      default_file_explorer = true,
+      buf_options = {
+        buflisted = false,
+        bufhidden = "hide",
+      },
+      win_options = {
+        wrap = false,
+        signcolumn = "no",
+        cursorcolumn = false,
+        foldcolumn = "0",
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = "nvic",
+      },
+      delete_to_trash = true,
+      prompt_save_on_select_new_entry = true,
+      view_options = {
+        show_hidden = false,
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, ".")
+        end,
+        is_always_hidden = function()
+          return false
+        end,
+        natural_order = true,
+        sort = {
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+      },
+      float = {
+        padding = 2,
+        max_width = 90,
+        max_height = 30,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      preview = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        max_height = 0.9,
+        min_height = { 5, 0.1 },
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        border = "rounded",
+        minimized_border = "none",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      use_default_keymaps = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+      confirmation = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        max_height = 0.9,
+        min_height = { 5, 0.1 },
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 }
+
